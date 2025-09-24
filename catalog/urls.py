85be511615ad640fb304d7ae31daa735d354ad46
GromazekaS <kinst41@gmail.com>
2025-09-24 22:19:54 +0300

    complete * task

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path ('', views.catalog_view, name='home'),
-    path ('contacts/', views.contacts_view, name='contacts')
+    # path ('contacts/', views.contacts_view, name='contacts'),
+    path ('contacts/', views.send_callback, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,4 +7,13 @@ def catalog_view(request):
 
 
 def contacts_view(request):
+    return render(request, 'catalog/contacts.html')
+
+
+def send_callback(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        print(f'Пользователь {name} оставил сообщение: {message}')
+        return HttpResponse(f'Спасибо, {name}! Ваше сообщение получено')
     return render(request, 'catalog/contacts.html')
