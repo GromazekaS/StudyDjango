25f5d326f887844d9e85ae2bbeffbc24e87171dc
GromazekaS <kinst41@gmail.com>
2025-09-28 18:13:54 +0300

    make migrations, install Pillow

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(null=True, blank=True)
     image =models.ImageField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE())
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price_per_item = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
