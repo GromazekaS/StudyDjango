bb8a90d3d72ca8ec49c28a2810afc36241dd0e19
GromazekaS <kinst41@gmail.com>
2025-10-13 22:12:41 +0300

    task 2 complete

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 namespace = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='posts_list'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('new/', views.PostCreateView.as_view(), name='post_create'),
     path('<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -8,34 +8,34 @@ from django.core.mail import send_mail
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog_example_page'
-    context_object_name = 'post'
+    template_name = 'blog/posts_list.html'
+    context_object_name = 'posts'
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog_example_page'
+    template_name = 'blog/full_post.html'
     context_object_name = 'post'
 
 
 class PostCreateView(CreateView):
     model = Post
     fields = ['title','text', 'thumbnail']
-    template_name = 'blog_example_page'
-    success_url = reverse_lazy('post_list')
+    template_name = 'blog/new_post.html'
+    success_url = reverse_lazy('posts_list')
 
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = ['title','text', 'thumbnail', 'is_published']
-    template_name = 'blog_example_page'
-    success_url = reverse_lazy('post_list')
+    template_name = 'blog/post_edit.html'
+    success_url = reverse_lazy('posts_list')
 
 
 class PostDeleteView(DeleteView):
     model = Post
-    template_name = 'blog_example_page'
-    success_url = reverse_lazy('post_list')
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('posts_list')
 
 
 from django.shortcuts import render
